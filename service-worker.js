f859587092f640c155831385ab359affa6ab9779
SATHIRI-SATMEC <jorgeluisleiva2020@gmail.com>
2026-06-24 21:55:34 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = "cartera-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  const url = e.request.url;
+  // nunca cachear las llamadas al Apps Script (datos en vivo)
+  if (url.includes("script.google.com")) return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(()=>caches.match("./index.html")))
+  );
+});
